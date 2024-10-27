47011d11a937e2bcd22568c2cb1865f16d5c6121
rvainshtein <rvainshtein@gmail.com>
2024-10-27 12:59:07 +0200

    added all files of old masked mimic repository, note that due to new structure of the code there's a hierarchical structure to envs/masked_mimic_inversion

---
+++ phys_anim/envs/masked_mimic_inversion/common/common_task.py
@@ -0,0 +1,83 @@
+# Copyright (c) 2020, NVIDIA CORPORATION.  All rights reserved.
+# NVIDIA CORPORATION and its licensors retain all intellectual property
+# and proprietary rights in and to this software, related documentation
+# and any modifications thereto.  Any use, reproduction, disclosure or
+# distribution of this software and related documentation without an express
+# license agreement from NVIDIA CORPORATION is strictly prohibited.
+
+from phys_anim.utils.motion_lib import MotionLib
+
+from typing import Optional, TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from phys_anim.envs.isaacgym.masked_mimic_inversion.task_humanoid import (
+        MaskedMimicTaskHumanoid,
+    )
+else:
+    MaskedMimicTaskHumanoid = object
+
+# TODO: heading, pacer path follower, location, reach
+#  task defines all parameters, by default they should be fully masked out.
+#  allow task to control joints in several forms, e.g., pelvis position in X frames or all frames, etc...
+#  see what works best, submit that. Just like PACER/ASE tried multiple rewards/paths until they figured
+#  what works best.
+
+# TODO: make sure no reset due to end of motion file, just keep going until the end of the episode.
+
+
+class BaseMaskedMimicTask(MaskedMimicTaskHumanoid):  # type: ignore[misc]
+    def __init__(self, config, device, motion_lib: Optional[MotionLib] = None):
+        super().__init__(config, device, motion_lib=motion_lib)
+        self.setup_task()
+
+    ###############################################################
+    # Set up environment
+    ###############################################################
+    def setup_task(self):
+        pass
+
+    ###############################################################
+    # Handle reset
+    ###############################################################
+    def reset_envs(self, env_ids):
+        super().reset_envs(env_ids)
+        if len(env_ids) > 0:
+            self.reset_task(env_ids)
+
+    def reset_task(self, env_ids):
+        # Make sure in user-control mode that the history isn't visible.
+        self.valid_hist_buf.set_all(False)
+
+    ###############################################################
+    # Environment step logic
+    ###############################################################
+    def compute_humanoid_obs(self, env_ids=None):
+        humanoid_obs = super().compute_humanoid_obs(env_ids)
+
+        # After the humanoid obs is called, we compute the task obs.
+        # A task obs will have its own unique tensor.
+        # We do not append the task obs to the humanoid obs, rather we allow the user to define the network structure
+        # and how the task obs is used in the network.
+        self.compute_task_obs()
+
+        return humanoid_obs
+
+    def compute_task_obs(self, env_ids=None):
+        pass
+
+    def mask_everything(self):
+        # By Default mask everything out. Individual tasks will override this.
+        self.masked_mimic_target_poses_masks[:] = False
+        self.masked_mimic_target_bodies_masks[:] = False
+        self.target_pose_obs_mask[:] = False
+        self.object_bounding_box_obs_mask[:] = False
+        self.motion_text_embeddings_mask[:] = False
+
+    def update_task(self, actions):
+        pass
+
+    ###############################################################
+    # Helpers
+    ###############################################################
+    def draw_task(self):
+        return
---
+++ phys_anim/envs/masked_mimic_inversion/isaacgym/path_following_humanoid.py
@@ -0,0 +1,115 @@
+from isaacgym import gymapi, gymtorch # type: ignore[misc]
+from isaac_utils import torch_utils
+
+import torch
+from torch import Tensor
+
+import numpy as np
+from typing import Optional
+
+from phys_anim.envs.isaacgym.masked_mimic_inversion.task_humanoid import MaskedMimicTaskHumanoid
+from phys_anim.envs.common.masked_mimic_inversion.common_path import BaseMaskedMimicPathFollowing
+from phys_anim.utils.motion_lib import MotionLib
+
+
+class MaskedMimicPathFollowingHumanoid(BaseMaskedMimicPathFollowing, MaskedMimicTaskHumanoid):  # type: ignore[misc]
+    def __init__(
+            self, config, device: torch.device, motion_lib: Optional[MotionLib] = None
+    ):
+        super().__init__(config=config, device=device, motion_lib=motion_lib)
+
+        if not self.headless:
+            self._build_marker_state_tensors()
+
+    ###############################################################
+    # Set up IsaacGym environment
+    ###############################################################
+    def create_envs(self, num_envs, spacing, num_per_row):
+        if not self.headless:
+            self._marker_handles = [[] for _ in range(num_envs)]
+            self._load_marker_asset()
+
+        super().create_envs(num_envs, spacing, num_per_row)
+
+    def _load_marker_asset(self):
+        asset_root = "phys_anim/data/assets/urdf/"
+        asset_file = "traj_marker.urdf"
+
+        asset_options = gymapi.AssetOptions()
+        asset_options.angular_damping = 0.01
+        asset_options.linear_damping = 0.01
+        asset_options.max_angular_velocity = 100.0
+        asset_options.density = 1.0
+        asset_options.fix_base_link = True
+        asset_options.default_dof_drive_mode = gymapi.DOF_MODE_NONE
+
+        self._marker_asset = self.gym.load_asset(self.sim, asset_root, asset_file, asset_options)
+
+    def build_env(self, env_id, env_ptr, humanoid_asset):
+        super().build_env(env_id, env_ptr, humanoid_asset)
+
+        if not self.headless:
+            self._build_marker(env_id, env_ptr)
+
+    def _build_marker(self, env_id, env_ptr):
+        default_pose = gymapi.Transform()
+
+        for i in range(self._num_traj_samples):
+
+            marker_handle = self.gym.create_actor(env_ptr, self._marker_asset, default_pose, "marker", self.num_envs + 10, 0, 0)
+            self.gym.set_rigid_body_color(env_ptr, marker_handle, 0, gymapi.MESH_VISUAL, gymapi.Vec3(0.8, 0.0, 0.0))
+            self._marker_handles[env_id].append(marker_handle)
+
+    def _build_marker_state_tensors(self):
+        num_actors = self.get_num_actors_per_env()
+        if self.total_num_objects > 0:
+            self._marker_states = self.root_states[:-self.total_num_objects].view(self.num_envs, num_actors, self.root_states.shape[-1])[..., 1:(1 + self._num_traj_samples), :]
+        else:
+            self._marker_states = self.root_states.view(self.num_envs, num_actors, self.root_states.shape[-1])[..., 1:(1 + self._num_traj_samples), :]
+        self._marker_pos = self._marker_states[..., :3]
+
+        self._marker_actor_ids = self.humanoid_actor_ids.unsqueeze(-1) + torch_utils.to_torch(self._marker_handles, dtype=torch.int32, device=self.device)
+        self._marker_actor_ids = self._marker_actor_ids.flatten()
+
+    ###############################################################
+    # Environment step logic
+    ###############################################################
+    def compute_observations(self, env_ids=None):
+        super().compute_observations(env_ids)
+
+
+    ###############################################################
+    # Helpers
+    ###############################################################
+    def _update_marker(self):
+        traj_samples = self.fetch_path_samples(time_offset=0)[0].clone()
+        self._marker_pos[:] = traj_samples
+        if not self.config.path_generator.height_conditioned:
+            self._marker_pos[..., 2] = 0.92  # CT hack
+
+        markers_global_positions = self.convert_to_global_coords(traj_samples[..., :2].view(self.num_envs, -1, 2), self.env_offsets[..., :2].view(self.num_envs, 1, 2)).view(-1, 2)
+        ground_below_marker = self.get_ground_heights(markers_global_positions).view(traj_samples.shape[:-1])
+
+        self._marker_pos[..., 2] += ground_below_marker
+
+        self.gym.set_actor_root_state_tensor_indexed(self.sim, gymtorch.unwrap_tensor(self.root_states),
+                                                     gymtorch.unwrap_tensor(self._marker_actor_ids),
+                                                     len(self._marker_actor_ids))
+
+    def draw_task(self):
+        cols = np.array([[1.0, 0.0, 0.0]], dtype=np.float32)
+
+        bodies_positions = self.get_body_positions()
+        env_global_positions = self.convert_to_global_coords(bodies_positions[:, 0, :2], self.env_offsets[..., :2])
+
+        self._update_marker()
+
+        for i, env_ptr in enumerate(self.envs):
+            verts = self.path_generator.get_traj_verts(i).clone()
+            if not self.config.path_generator.height_conditioned:
+                verts[..., 2] = self.humanoid_root_states[i, 2]  # ZL Hack
+            else:
+                verts[..., 2] += self.get_ground_heights(env_global_positions)[i]
+            lines = torch.cat([verts[:-1], verts[1:]], dim=-1).cpu().numpy()
+            curr_cols = np.broadcast_to(cols, [lines.shape[0], cols.shape[-1]])
+            self.gym.add_lines(self.viewer, env_ptr, lines.shape[0], lines, curr_cols)
