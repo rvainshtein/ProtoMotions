8eee59035a3f5959cd09ac7097b185526562c6d8
rvainshtein <rvainshtein@gmail.com>
2024-12-07 16:43:13 +0200

    added warmup to best reward model checkpoints

--- phys_anim/agents/callbacks/save_best_reward_model.py
+++ phys_anim/agents/callbacks/save_best_reward_model.py
@@ -10,11 +10,12 @@ class SaveBestModelCallback:
     training_loop: PPO
     env: BaseHumanoid
 
-    def __init__(self):
+    def __init__(self, warmup: int = 20):
         """
         Callback to save the model when rewards exceed the previous best.
         """
         self.best_reward = float('-inf')
+        self.warmup = warmup
 
     def after_train(self, training_loop: PPO):
         """
@@ -23,14 +24,16 @@ class SaveBestModelCallback:
         Args:
             training_loop (PPO): The trainer instance.
         """
-        # Assuming `self.experience_buffer.total_rewards` is a tensor
-        current_reward = training_loop.experience_buffer.total_rewards.mean().item()
+        if training_loop.current_epoch < self.warmup:
+            # Skip the first few epochs
+            return
 
+        current_reward = training_loop.experience_buffer.total_rewards.mean().item()
         if current_reward > self.best_reward:
             self.best_reward = current_reward
             print(f"New best reward: {self.best_reward}. Saving model...")
 
             # Save the model to the specified directory
             ckpt_name = "best_model.ckpt"
-            training_loop.save(name=ckpt_name)  #  the path is None, so it will save to the default directory
+            training_loop.save(name=ckpt_name)  # the path is None, so it will save to the default directory
             print(f"Model saved with reward: {self.best_reward}")
