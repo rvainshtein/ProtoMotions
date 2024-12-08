487c3e6d73a7f3e37d72b05f038e13c79d598e4a
rvainshtein <rvainshtein@gmail.com>
2024-12-08 12:05:31 +0200

    wip: direction facing fix

--- phys_anim/envs/masked_mimic_inversion/direction_facing/common.py
+++ phys_anim/envs/masked_mimic_inversion/direction_facing/common.py
@@ -1,8 +1,11 @@
+import numpy as np
 import torch
 from isaac_utils import torch_utils, rotations
 from isaac_utils.rotations import quat_rotate
 from isaac_utils.torch_utils import calc_heading_quat
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Dict
+
+from torch import Tensor
 
 from phys_anim.envs.masked_mimic_inversion.steering.common import compute_heading_reward
 
@@ -34,6 +37,42 @@ class MaskedMimicBaseDirectionFacing(MaskedMimicDirectionFacingHumanoid):  # typ
         facing_obs = compute_facing_observations(root_states, self._tar_dir, self.w_last)
         self.inversion_obs = torch.cat([self.direction_obs, facing_obs], dim=-1)
 
+    def compute_reward(self, actions):
+        root_pos = self.get_humanoid_root_states()[..., :3]
+        self.rew_buf[:], output_dict = compute_facing_reward(
+            root_pos, self._prev_root_pos, self._tar_dir, self._tar_speed, self.dt
+        )
+        self._prev_root_pos[:] = root_pos
+
+        # print the target speed of the env and the speed actually achieved in that direction
+
+        if self.config.num_envs == 1 and self.config.steering_params.log_speed and self.progress_buf % 3 == 0:
+            print(f'speed: {output_dict["tar_dir_speed"].item():.3f}/{self._tar_speed.item():.3f}')
+            print(
+                f'error: {output_dict["tar_vel_err"].item():.3f}; tangent error: {output_dict["tangent_vel_err"].item():.3f}')
+
+        other_log_terms = {
+            "total_rew": self.rew_buf,
+        }
+
+        for rew_name, rew in other_log_terms.items():
+            self.log_dict[f"{rew_name}_mean"] = rew.mean()
+            # self.log_dict[f"{rew_name}_std"] = rew.std()
+
+        self.last_unscaled_rewards: Dict[str, Tensor] = self.log_dict
+        self.last_other_rewards = other_log_terms
+
+    def reset_heading_task(self, env_ids):
+        super().reset_heading_task(env_ids)
+        n = len(env_ids)
+        if np.random.binomial(1, self._random_heading_probability):
+            rand_face_theta = 2 * torch.pi * torch.rand(n, device=self.device) - torch.pi
+        else:
+            rand_face_theta = torch.zeros(n, device=self.device)
+
+        face_tar_dir = torch.stack([torch.cos(rand_face_theta), torch.sin(rand_face_theta)], dim=-1)
+        self._tar_facing_dir[env_ids] = face_tar_dir
+        return
 
 @torch.jit.script
 def compute_facing_observations(root_states, tar_face_dir, w_last: bool):
@@ -45,7 +84,7 @@ def compute_facing_observations(root_states, tar_face_dir, w_last: bool):
     return local_tar_face_dir
 
 
-# @torch.jit.script
+@torch.jit.script
 def compute_facing_reward(root_pos, prev_root_pos, root_rot, tar_dir, tar_speed, tar_face_dir, dt):
     # type: (Tensor, Tensor, Tensor, Tensor, Tensor, Tensor, float) -> Tensor
     dir_reward, output_dict = compute_heading_reward(root_pos, prev_root_pos, tar_dir, tar_speed, dt)
