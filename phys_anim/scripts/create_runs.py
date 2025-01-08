de868d7780534b4ec30afa12051f4a0d4e509b4b
rvainshtein <rvainshtein@gmail.com>
2025-01-08 14:23:09 +0200

    fix in create_runs.py

--- phys_anim/scripts/create_runs.py
+++ phys_anim/scripts/create_runs.py
@@ -1,6 +1,7 @@
 def main():
     output_file_path = 'runs.txt'
     base_run_command = 'python phys_anim/train_agent.py +robot=smpl +backbone=isaacgym +opt=[full_run,wdb]'
+    base_run_command += ' algo.config.max_epochs=4000 seed=${seed} wandb.wandb_group=phys_inversion wandb.wandb_project=chens_runs'
     experiment_arg = '+exp=inversion/{}'
     extra_args = '{}'
 
@@ -9,6 +10,8 @@ def main():
     use_text = [True, False]
     use_current_pose_obs = [True, False]
     use_bigger_model = [True, False]
+
+
     # create runs for each combination of hyperparameters
     for env in envs:
         for prior_flag in use_chens_prior:
@@ -19,16 +22,14 @@ def main():
                         current_run_command += base_run_command
                         current_exp = experiment_arg.format(env)
                         experiment_name = f'{env}_prior_{prior_flag}_text_{text_flag}_current_pose_{current_pose}_bigger_{bigger_model}'
-                        current_run_command += f' +experiment_name={experiment_name}'
-                        if bigger_model:
-                            current_run_command += ' algo.config.models.extra_input_model_for_transformer.config.units = [512, 512, 512]'
+                        current_run_command += f' +experiment_name={experiment_name}' + '_${seed}'
                         extra_args = f' +env.config.use_chens_prior={prior_flag}'
                         extra_args += f' +env.config.use_text={text_flag}'
                         if current_pose:
                             extra_args += f'+env.config.use_current_pose_obs={current_pose}'
-                            extra_args += ' algo.config.models.extra_input_obs_size = 11'
+                            extra_args += ' algo.config.models.extra_input_obs_size=11'
                         if bigger_model:
-                            extra_args += ' algo.config.models.extra_input_model_for_transformer.config.units = [512, 512, 512]'
+                            extra_args += ' algo.config.models.extra_input_model_for_transformer.config.units=[512,512,512]'
                         current_run_command = ' '.join([current_run_command, current_exp, extra_args])
                         with open(output_file_path, 'a') as f:
                             f.write(current_run_command + '\n')
