9dc05669b0e103f86b48e65f7075171c8961f813
rvainshtein <rvainshtein@gmail.com>
2025-01-09 15:52:27 +0200

    create_runs.py - fix cli arg order

--- phys_anim/scripts/create_runs.py
+++ phys_anim/scripts/create_runs.py
@@ -7,17 +7,11 @@ def main():
     if os.path.exists(output_file_path):
         os.remove(output_file_path)
     base_run_command = "python phys_anim/train_agent.py +robot=smpl +backbone=isaacgym"
+    extra_args = []
     max_epochs = 20 if DEBUG else 4000
-    base_run_command += f" algo.config.max_epochs={max_epochs}"
     base_run_command += " seed=${seed}" if not DEBUG else ""
     if not DEBUG:
-        base_run_command += " wandb.wandb_entity=phys_inversion wandb.wandb_project=chens_runs"
-    else:
-        base_run_command += ""
-
-    experiment_arg = "+exp=inversion/{}"
-
-    extra_args = []
+        extra_args += ["wandb.wandb_entity=phys_inversion wandb.wandb_project=chens_runs"]
 
     if DEBUG:
         opts = ["small_run", "wdb", ]
@@ -42,12 +36,12 @@ def main():
                         current_extra_args = extra_args.copy()
                         current_run_command = ""
                         current_run_command += base_run_command
-                        current_exp = experiment_arg.format(env)
+                        current_run_command += f' +exp=inversion/{env}'
                         current_experiment_name = f"{env}_prior_{prior_flag}_text_{text_flag}_current_pose_{current_pose}_bigger_{bigger_model}"
 
                         if DEBUG:
                             current_experiment_name += '_DEBUG'
-
+                            current_extra_args += [f"algo.config.max_epochs={max_epochs}"]
                         current_run_command += (
                                 f" experiment_name={current_experiment_name}" + "_${seed}"
                         )
@@ -61,7 +55,7 @@ def main():
                         opt_string = "+opt=[" + ','.join(current_opts) + "]"
                         extra_args_string = ' '.join(current_extra_args)
                         current_run_command = " ".join(
-                            [current_run_command, opt_string, current_exp, extra_args_string, ]
+                            [current_run_command, opt_string, extra_args_string, ]
                         )
 
                         with open(output_file_path, "a") as f:
