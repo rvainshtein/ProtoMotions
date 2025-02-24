39e03361c2b5a27bbe60e9a59e70924d03b17ef6
ronv <rvainshtein@gmail.com>
2025-02-24 13:26:56 +0200

    all_runs.py: added seed option to recordings, refactored getting checkpoints_args to allow turning on and off running on all the reach runs

--- phys_anim/scripts/exps/all_runs.py
+++ phys_anim/scripts/exps/all_runs.py
@@ -6,30 +6,34 @@ from typing import List
 import numpy as np
 
 
-def get_checkpoints_arg(base_dir, env_name, perturbation, prior_only, record_video):
+def get_checkpoints_arg(base_dir, env_name, prior_only, record_video, record_seed=0, run_all_reach=True):
+    seed_str = f"_{record_seed}" if record_video else ""
     if prior_only:
         checkpoints_arg = glob.glob(f"{base_dir}/{env_name}/*"
                                     f"_prior_True_text_False_current_pose_True_bigger_True_train_actor_False_0/"
                                     f"last.ckpt")[0]
         checkpoints_arg = f"+checkpoint_paths=[{checkpoints_arg}]"
+
+    elif env_name == 'reach':
+        checkpoints_arg = get_reach_checkpoints_arg(base_dir, env_name, record_seed, record_video, run_all_reach)
     else:
-        if env_name != 'reach' or (env_name == 'reach' and perturbation == "None" and not record_video):
-            if record_video:
-                checkpoints_arg = f"+checkpoint_paths_glob={base_dir}/{env_name}/*_0/last.ckpt"
-            else:
-                checkpoints_arg = f"+checkpoint_paths_glob={base_dir}/{env_name}/*/last.ckpt"
-        else:
-            reach_runs = get_valid_reach_runs(base_dir, f'reach/*/last.ckpt',
-                                              f'inversion_direction_facing/*/last.ckpt')
-            if record_video:
-                reach_runs = [run + '/last.ckpt' for run in reach_runs if '_0/' in run]
-            else:
-                reach_runs = [run + '/last.ckpt' for run in reach_runs]
-            checkpoints_arg = f"+checkpoint_paths=[{','.join(reach_runs)}]"
+        checkpoints_arg = f"+checkpoint_paths_glob={base_dir}/{env_name}/*{seed_str}/last.ckpt"
     return checkpoints_arg
 
 
-def get_valid_reach_runs(base_dir: str, reach_runs_glob: str, other_env_runs_glob: str) -> List[str]:
+def get_reach_checkpoints_arg(base_dir, env_name, record_seed, record_video, run_all_reach):
+    seed_str = f"_{record_seed}" if record_video else ""
+    if run_all_reach:
+        checkpoints_arg = f"+checkpoint_paths_glob={base_dir}/{env_name}{seed_str}/*/last.ckpt"
+    else:
+        reach_runs = get_common_reach_runs(base_dir, f'reach/*/last.ckpt',
+                                           f'inversion_direction_facing/*/last.ckpt')
+        reach_runs = [run + '/last.ckpt' for run in reach_runs if f'{seed_str}' in run]
+        checkpoints_arg = f"+checkpoint_paths=[{','.join(reach_runs)}]"
+    return checkpoints_arg
+
+
+def get_common_reach_runs(base_dir: str, reach_runs_glob: str, other_env_runs_glob: str) -> List[str]:
     # get names of checkpoints in other env
     other_env_runs = glob.glob(os.path.join(base_dir, other_env_runs_glob))
     # get names of checkpoints in reach env
@@ -74,6 +78,8 @@ def generate_eval_checkpoints_bash():
     # num_envs = 20
     games_per_env = 1
     # games_per_env = 5
+    record_seed = 0
+    run_all_reach = True
 
     ## DEBUG
     # output_file = "debug_eval_runs"
@@ -97,9 +103,12 @@ def generate_eval_checkpoints_bash():
     record_video = True
     num_envs = 20
     games_per_env = 1
-    gpu_ids = [0]
+    gpu_ids = [0, 1]
     record_dir = "output/FINALLY_"
     termination = False
+    record_seed = 2
+    run_all_reach = False
+
     #
 
     # # RE-RUN steering + direction_facing
@@ -133,7 +142,7 @@ def generate_eval_checkpoints_bash():
 
                 cmd = generate_cmd(base_dir, cluster_base_dir, env_name, games_per_env, gpu_ids, num_envs,
                                    perturbation_name, perturbation_val, prior_only, project_prefix, record_dir,
-                                   record_video, termination)
+                                   record_video, termination, run_all_reach=run_all_reach, record_seed=record_seed)
                 # subprocess.run(cmd, check=True)
                 all_cmds.append(' '.join(cmd))
     with open(os.path.join(out_dir, f'{output_file}_{"record_" if record_video else ""}.sh'), 'w') as f:
@@ -141,8 +150,11 @@ def generate_eval_checkpoints_bash():
 
 
 def generate_cmd(base_dir, cluster_base_dir, env_name, games_per_env, gpu_ids, num_envs, perturbation_name,
-                 perturbation_val, prior_only, project_prefix, record_dir, record_video, termination):
-    checkpoints_arg = get_checkpoints_arg(base_dir, env_name, perturbation_name, prior_only, record_video)
+                 perturbation_val, prior_only, project_prefix, record_dir, record_video, termination, record_seed=0,
+                 run_all_reach=True):
+    checkpoints_arg = get_checkpoints_arg(base_dir, env_name, prior_only, record_video,
+                                          run_all_reach=run_all_reach,
+                                          record_seed=record_seed)
     project_name = generate_project_name(env_name, perturbation_name,
                                          prefix=project_prefix,
                                          per_env_perturbation=False)
@@ -191,10 +203,12 @@ def generate_perturbations_bash():
         # "inversion_strike",
         # "inversion_long_jump"
     ]
+    run_all_reach = True
     record_video = False
     termination = True
     num_envs = 1 * 1024
     games_per_env = 1
+    record_seed = 0
 
     # PERTURBATIONS
     gravity_log_space = np.array([[0.25, 0.4, 0.6, 0.75, 1, 1.15, 1.3, 1.5, 1.6, 1.75, 2]]) * -9.81
@@ -216,7 +230,7 @@ def generate_perturbations_bash():
             for env_name in envs_names_list:
                 cmd = generate_cmd(base_dir, cluster_base_dir, env_name, games_per_env, gpu_ids, num_envs,
                                    perturbation_name, perturbation_val, prior_only, project_prefix, record_dir,
-                                   record_video, termination)
+                                   record_video, termination, run_all_reach=run_all_reach, record_seed=record_seed)
                 all_cmds.append(' '.join(cmd))
 
     with open(os.path.join(out_dir, f'{output_file}_{"record_" if record_video else ""}.sh'), 'w') as f:
