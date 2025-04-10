18fdce9b17e6ab720f395a793aa155da02e2796e
zohar <zohar.rimon@gmail.com>
2025-04-10 18:38:22 +0300

    Removed docker

--- docker/it.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-if hash nvidia-docker 2>/dev/null; then
-  cmd=nvidia-docker
-else
-  cmd=docker
-fi
-
-${cmd} exec -it "$(docker ps --filter ancestor=masked_mimic:v1 --format "{{.Names}}")" bash
